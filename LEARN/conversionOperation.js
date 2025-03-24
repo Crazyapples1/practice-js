7137b56f007f3b26212225b69c47cb44cb62b7f7
Crazyapples1 <118990354+Crazyapples1@users.noreply.github.com>
2025-03-24 14:27:56 +0000

    conversion updated

--- LEARN/conversionOperation.js
+++ LEARN/conversionOperation.js
@@ -1,16 +1,16 @@
 let score1 = "33"
 
-console.log(typeof score1);
+//console.log(typeof score1);
 //OR
-console.log(typeof(score1)); //dont use this
+//console.log(typeof(score1)); //dont use this
 
 let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber1);
+//console.log(typeof valueInNumber1);
 
 let score2 = "3abc"
 let valueInNumber2 = Number(score2)
-console.log(typeof valueInNumber2); // number
-console.log(valueInNumber2); //Nan => not a number. Therefore check before conversion
+//console.log(typeof valueInNumber2); // number
+//console.log(valueInNumber2); //Nan => not a number. Therefore check before conversion
 /*
 If convert null to number then value = 0
 and if undefined value = NaN
@@ -26,5 +26,10 @@ let valueInBoolean = Boolean(isLoggedIn)
 // string(not empty) => true
 let someNumber = 33
 let valueInString = String(someNumber)
-console.log(typeof valueInString);
+//console.log(typeof valueInString);
+
+// ---------------------Operations----------------------
+
+let value = 3
+let negvalue = -value
 
